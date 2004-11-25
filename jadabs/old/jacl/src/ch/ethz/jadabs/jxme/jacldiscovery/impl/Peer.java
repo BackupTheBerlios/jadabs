8dc41e811b16e456eb57bf6914a9f3edb7f7d167
afrei <afrei>
2004-11-25 16:36:08 +0000

    move jacl to old code

---
+++ jadabs/old/jacl/src/ch/ethz/jadabs/jxme/jacldiscovery/impl/Peer.java
@@ -0,0 +1,14 @@
+package ch.ethz.jadabs.jxme.jacldiscovery.impl;
+
+/**
+ * 
+ * @author Jan S. Rellermeyer, jrellermeyer_at_student.ethz.ch
+ *
+ * Encapsulates the information about a peer.
+ */
+public class Peer {
+	public String name;
+	public long lastAlive;	
+	public long offset;
+	public float prob;	
+}
