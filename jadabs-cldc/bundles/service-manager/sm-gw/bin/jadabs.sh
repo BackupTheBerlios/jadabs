7ff95e47425201683b98fad486020665e9eb816b
afrei <afrei>
2005-04-26 16:25:28 +0000

    update jadabs start script

---
+++ jadabs-cldc/bundles/service-manager/sm-gw/bin/jadabs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+XARGS=init.xargs
+
+PWD=$(pwd)
+PROSE_DIR=prose-1.2.1
+
+export LD_LIBRARY_PATH=$PWD/$PROSE_DIR/lib/i386
+
+if [ -n "$1" ]; then XARGS=$1; fi
+
+if [ -z $2 ]; then
+	java -Dorg.knopflerfish.gosg.jars=file:$HOME/.maven/repository/ -Dch.ethz.jadabs.jxme.peeralias=btgw -jar $HOME/.maven/repository/osgi/jars/framework-aop-1.3.3.jar -xargs $XARGS
+else
+	java -Xdebug -Xnoclassgc -Xbootclasspath/a:$PROSE_DIR/lib/jdk-jvmai-loc.jar -Dprose.library.path=$PWD/$PROSE_DIR/lib/i386 -Xrunprosevm -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n -Dch.ethz.inf.project.home=$PWD/$PROSE_DIR -Dprose.port:5000 -Dorg.knopflerfish.gosg.jars=file:./repository/ -Dch.ethz.jadabs.jxme.peeralias=$HOSTNAME -cp $PROSE_DIR/lib/jdk-prose-loc.jar:framework-aop-1.3.3.jar org.knopflerfish.framework.Main -xargs $XARGS
+fi
--- jadabs-cldc/bundles/service-manager/sm-gw/bin/runpeer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-XARGS=init.xargs
-
-if [ -n "$1" ]; then XARGS=$1; fi
-
-java -Dch.ethz.jadabs.jxme.peeralias=btgw -Dorg.knopflerfish.gosg.jars=file:$HOME/.maven/repository/ -jar $HOME/.maven/repository/osgi/jars/framework-1.3.3.jar -xargs $XARGS
