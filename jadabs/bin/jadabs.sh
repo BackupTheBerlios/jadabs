5b55669eea141dc1d137967396ee38e6145b5da8
afrei <afrei>
2005-06-21 13:06:31 +0000

    fix startup

--- jadabs/bin/jadabs.sh
+++ jadabs/bin/jadabs.sh
@@ -5,12 +5,12 @@ XARGS=init.xargs
 PWD=$(pwd)
 PROSE_DIR=prose-1.2.1
 
+FRAMEWORK=framework-aop-1.3.3.jar
+
 export LD_LIBRARY_PATH=$PWD/$PROSE_DIR/lib/i386
 
-if [ -n "$1" ]; then XARGS=$1; fi
-
-if [ -z $2 ]; then
-	java -Dorg.knopflerfish.gosg.jars=file:./repository/ -Dch.ethz.jadabs.jxme.peeralias=$HOSTNAME -jar framework-aop-1.3.3.jar -xargs $XARGS
+if [ -z $1 ]; then
+	java -Dorg.knopflerfish.gosg.jars=file:repository/ -Dch.ethz.jadabs.jxme.peeralias=$HOSTNAME -jar $FRAMEWORK -xargs $XARGS
 else
-	java -Xdebug -Xnoclassgc -Xbootclasspath/a:$PROSE_DIR/lib/jdk-jvmai-loc.jar -Dprose.library.path=$PWD/$PROSE_DIR/lib/i386 -Xrunprosevm -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n -Dch.ethz.inf.project.home=$PWD/$PROSE_DIR -Dprose.port:5000 -Dorg.knopflerfish.gosg.jars=file:./repository/ -Dch.ethz.jadabs.jxme.peeralias=$HOSTNAME -cp $PROSE_DIR/lib/jdk-prose-loc.jar:framework-aop-1.3.3.jar org.knopflerfish.framework.Main -xargs $XARGS
+	java -Xdebug -Xnoclassgc -Xbootclasspath/a:$PROSE_DIR/lib/jdk-jvmai-loc.jar -Dprose.library.path=$PWD/$PROSE_DIR/lib/i386 -Xrunprosevm -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n -Dch.ethz.inf.project.home=$PWD/$PROSE_DIR -Dprose.port:5000 -Dorg.knopflerfish.gosg.jars=file:repository/ -Dch.ethz.jadabs.jxme.peeralias=$HOSTNAME -cp $PROSE_DIR/lib/jdk-prose-loc.jar:$FRAMEWORK org.knopflerfish.framework.Main -xargs $XARGS
 fi
